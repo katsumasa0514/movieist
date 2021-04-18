7bd38765f40505b55509a227d049601b091a86b8
k-nish <koh.oct.un@gmail.com>
2021-04-18 19:23:33 +0900

    [add] remove DS_Store & add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages
+from setuptools import setup
+
+
+setup_requires = []
+install_requires = [
+]
+
+setup(
+    name='movieist',
+    version='0.0.1',
+    description='',
+    author='',
+    author_email='',
+    url='',
+    license='',
+    packages=find_packages(),
+    zip_safe=False,
+    setup_requires=setup_requires,
+    install_requires=install_requires,
+)
